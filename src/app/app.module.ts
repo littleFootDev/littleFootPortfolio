b5db77f559b27a0c21a90e04bcf650d5517f5531
LittleFootDev <thelittlefootdev@gmail.com>
2022-06-16 10:34:16 +0200

    structure of project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutMeComponent,
+    PortfolioComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,

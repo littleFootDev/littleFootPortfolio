168296d153b1d511b66b81d92f5f6bbf5853a166
LittleFootDev <thelittlefootdev@gmail.com>
2022-06-16 13:31:03 +0200

    adding header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  // toggler : HTMLInputElement | null= document.querySelector(".hamburger");
+  // navLinkContainer = document.querySelector(".navlinks-container");
+  navIsOpen = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggleNav() {
+   this.navIsOpen = !this.navIsOpen;
+  }
 }
